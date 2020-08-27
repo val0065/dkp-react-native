81a9d552a226d854bf7f3bf4c30ca638dcbedd3d
david <woozie28@gmail.com>
2020-08-27 09:01:12 +0200

    init

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  return(
+    <View style={styles.container}>
+      <Header title='DKP List'/>
+    </View>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+export default App;
